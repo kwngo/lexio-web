4be53bc13af1719284a0443080884e4002e65225
kwngo <kareem.kwong@gmail.com>
2020-05-02 20:08:14 -0400

    Add a footer

--- pages/index.js
+++ pages/index.js
@@ -43,8 +43,38 @@ const Home = () => (
       </div>
     </section>
     <footer className="footer">
-      <div className="content has-text-centered">
-        <span>I'm the footer</span>
+      <div class="container footer-container">
+        <div className="columns footer-content">
+          <div className="column">
+            <p className="footer-head">Company</p>
+            <p><a className="footer-link">Home</a></p>
+            <p><a className="footer-link">About</a></p>
+            <p><a className="footer-link">Careers</a></p>
+            <p><a className="footer-link">Partners</a></p>
+            <p><a className="footer-link">Blog</a></p>
+          </div>
+          <div className="column">
+            <p className="footer-head">Product</p>
+            <p><a className="footer-link">Pricing</a></p>
+            <p><a className="footer-link">Marketplace</a></p>
+          </div>
+          <div className="column">
+            <p className="footer-head">Education</p>
+            <p><a className="footer-link">Documentation</a></p>
+          </div>
+          <div className="column">
+            <p className="footer-head">More</p>
+            <p><a className="footer-link">Security</a></p>
+            <p><a className="footer-link">Contact Us</a></p>
+          </div>
+          <div className="column">
+            <p className="footer-head">Legal</p>
+            <p><a className="footer-link">Privacy Policy</a></p>
+            <p><a className="footer-link">Terms of Service</a></p>
+            <p><a className="footer-link">SLA</a></p>
+          </div>
+        </div>
+        <p>Copyright © 2020 Lexio Inc. All rights reserved.</p>
       </div>
     </footer>
   </Layout>
